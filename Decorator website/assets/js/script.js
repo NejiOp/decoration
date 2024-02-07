437b6965cab50da0ea657f453de16bf3eeebcc0c
ANURAG MISHRA <114349668+NejiOp@users.noreply.github.com>
2024-02-07 17:02:47 +0530

    Add files via upload

--- Decorator website/assets/js/script.js
+++ Decorator website/assets/js/script.js
@@ -195,3 +195,17 @@ window.addEventListener("mousemove", function (event) {
     parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
   }
 });
+
+const panels = document.querySelectorAll(".panel");
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
